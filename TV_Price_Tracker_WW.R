311b5759678a782abb050aff64d9224b9c176198
Leung <Marcus.Leung@ihs.com>
2017-01-18 14:28:03 +0000

    1

---
+++ TV_Price_Tracker_WW.R
@@ -0,0 +1,9 @@
+if(is.na(match(c("devtools"),installed.packages()[,"Package"]))) install.packages(new.packages) else library(devtools)
+suppressMessages(devtools::install_github("marcuskhl/BasicSettings"));suppressMessages(library(BasicSettings))
+dev.pkgs()
+
+con <- odbcConnect("TV_Price_Tracker_db")
+
+sqlTables(con, tableType = "TABLE", tableName = "Pricing_Output")$TABLE_NAME
+sqlColumns(con, "Pricing_Output")$COLUMN_NAME
+x <- sqlFetch(con, "Pricing_Output")
